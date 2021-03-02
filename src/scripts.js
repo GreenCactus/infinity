bdb84470fe86274ed65d7400b59746dbb197f559
Green Cactus <green.cactus.in.the.desert@gmail.com>
2021-03-02 18:56:54 +0300

    123

---
+++ src/scripts.js
@@ -0,0 +1,15 @@
+import { LEVELS } from "./config/levels";
+import World from "./game/world";
+
+document.addEventListener("deviceready", deviceReady, false);
+
+function deviceReady() {
+    console.log("Скрипты подключены");
+    const ratio = 1.5;
+    const world = new World({
+        width: window.innerWidth * ratio,
+        height: window.innerHeight * ratio
+    },LEVELS.DEV);
+
+    world.start();
+}
